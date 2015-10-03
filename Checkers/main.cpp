cd3b24fceffac252c219c118fd6c7f7d144f81c0
Johnny Chen <j_chen187@yahoo.com>
2015-10-03 11:39:56 -0400

    The project folder is initialized.

---
+++ Checkers/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Checkers
+//
+//  Created by Johnny Chen on 10/3/15.
+//  Copyright (c) 2015 JohnnyChen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
