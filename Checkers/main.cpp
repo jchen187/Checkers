6f27d6b85a8750d72c9cf56e76bbbca544e4bfb4
Johnny Chen <j_chen187@yahoo.com>
2015-10-21 22:18:37 -0400

    It now takes in a response.

--- Checkers/main.cpp
+++ Checkers/main.cpp
@@ -8,15 +8,31 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[]) {
 
     //Ask if want to start from regular, some file or input own coordinates
-    std::cout << "Choose a number from 1 - 3.\n"
+    cout << "How would you like to start?\n"
                 << "1. Start with the original board.\n"
                 << "2. Input your own file.\n"
-                << "3. Input your own coordinates.\n";
+                << "3. Input your own coordinates.\n"
+                << "Please choose a number from 1 - 3." << endl;
     //Get the user input. Must be a number
-    
+    int choice;
+    cin >> choice;
+    if (choice > 0 && choice < 4){
+        if (choice == 1){
+            
+        }
+        else if (choice == 2){
+            
+        }
+        else {
+            //ask for inputs
+        }
+    }
+
     return 0;
 }
 
