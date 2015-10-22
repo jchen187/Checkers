3b2aa4eef69e74b3ebf89823f6da5a5a7053bc55
Johnny Chen <j_chen187@yahoo.com>
2015-10-21 23:42:45 -0400

    It now prints the board, a simpler version

--- Checkers/main.cpp
+++ Checkers/main.cpp
@@ -2,13 +2,17 @@
 //  main.cpp
 //  Checkers
 //
-//  Created by Johnny Chen on 10/3/15.
-//  Copyright (c) 2015 JohnnyChen. All rights reserved.
+//  Created by Johnny Chen on 1./3/15.
+//  Copyright (c) 2.15 JohnnyChen. All rights reserved.
 //
 
 #include <iostream>
 
 using namespace std;
+
+void drawBoard(int x[][4]);
+
+
 
 int main(int argc, const char * argv[]) {
 
@@ -18,15 +22,44 @@ int main(int argc, const char * argv[]) {
                 << "2. Input your own file.\n"
                 << "3. Input your own coordinates.\n"
                 << "Please choose a number from 1 - 3." << endl;
+    /*
+        .1.1.1.1    1111
+        1.1.1.1.    1111
+        .1.1.1.1    1111
+        0.0.0.0.    0000
+        .0.0.0.0    0000
+        2.2.2.2.    2222
+        .2.2.2.2    2222
+        2.2.2.2.    2222
+     */
+    
+    const unsigned int height = 8;
+    const unsigned int width = 4;
+    int standardBoard[height][width];
+    for (int i = 0; i < 8; i++){
+        for (int j = 0; j < 4; j++){
+            if (i < 3){
+                standardBoard[i][j] = 1;
+            }
+            else if (i < 5)
+                standardBoard[i][j] = 0;
+            else {
+                standardBoard[i][j] = 2;
+            }
+        }
+    }
+    
     //Get the user input. Must be a number
     int choice;
     cin >> choice;
     if (choice > 0 && choice < 4){
         if (choice == 1){
-            
+            //show them the real board
+            drawBoard(standardBoard);
         }
         else if (choice == 2){
-            
+            //ask to put file
+            //read the file
         }
         else {
             //ask for inputs
@@ -37,3 +70,26 @@ int main(int argc, const char * argv[]) {
 }
 
 //draw board
+void drawBoard(int x[][4]){
+    for (int i = 0; i < 8; i++){
+        if (i % 2 == 0){
+            cout << ".";
+        }
+        if (i < 3){
+            for (int j = 0; j < 4; j++){
+                cout << "1." ;
+            }
+        }
+        else if (i < 5){
+            for (int j = 0; j < 4; j++){
+                cout << "0.";
+            }
+        }
+        else {
+            for (int j = 0; j < 4; j++){
+                cout << "2.";
+            }
+        }
+        cout << "\n";
+    }
+}
